0406e72228e0eb6fb57dfd547555ae468a7b46b3
Sharad Banga <sharadiit03@gmail.com>
2025-02-28 17:38:34 +0530

    update

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextFunction ,Request,Response } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+
+export const userMiddleware = (req:Request , res:Response ,next :NextFunction)=>{
+
+  console.log("sss");
+  
+
+  const header = req.headers["authorization"];
+  console.log(header);
+  
+  const decoded = jwt.verify(header as string,JWT_SECRET);
+
+  if(decoded){
+    //@ts-ignore
+    req.userId = decoded.id;
+    next();
+  }
+  else{
+    res.status(403).json({
+      message : "you are not logged in"
+    })
+  }
+
+}
